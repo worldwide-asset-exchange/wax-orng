e92e7794e582cf0997bd54d6cb1af9882fe3fdd7
mauri.opskins <52429090+mauri-opskins@users.noreply.github.com>
2019-08-09 17:19:29 -0300

    [KEW-1451] change receiverand to receive sha256

--- tests/contracts/randreceiver.cpp
+++ tests/contracts/randreceiver.cpp
@@ -35,27 +35,31 @@ public:
         , results_table(receiver, receiver.value) {
     }
 
-    ACTION receiverand(uint64_t assoc_id, const std::string& random_value) {
-        print_f("receiverand called: assoc_id=%, random_value=%\n", assoc_id, random_value.c_str());
+    ACTION receiverand(uint64_t assoc_id, const eosio::checksum256& random_value) {
+        auto rv_byte_array = random_value.extract_as_byte_array();
+        auto rv_string = bytes_to_string(rv_byte_array.data(), rv_byte_array.size());
+
+        print_f("receiverand called: assoc_id=%, random_value=%\n", assoc_id, rv_string );
+
         set_last_result(assoc_id, random_value);
     }
 
     ACTION resetresult() {
-        set_last_result(0, "");
+        set_last_result(0, eosio::checksum256());
     }
 
 private:
     TABLE results {
         uint64_t    id;
         uint64_t    assoc_id;
-        std::string random_value;
+        eosio::checksum256 random_value;
 
         auto primary_key() const { return id; }
     };
     
     multi_index<"results"_n, results> results_table;
 
-    void set_last_result(uint64_t assoc_id, const std::string& random_value) {
+    void set_last_result(uint64_t assoc_id, const eosio::checksum256& random_value) {
         auto it = results_table.find(0);
 
         if (it == results_table.end()) {
@@ -71,6 +75,17 @@ private:
                 rec.random_value = random_value;
             });
         }
+    }
+
+
+    static std::string bytes_to_string(const unsigned char* in, std::size_t size) {
+        std::string out;
+        const char* hex = "0123456789abcdef";
+        for (std::size_t i = 0; i < size; i++) {
+            out += hex[(in[i]>>4) & 0xF];
+            out += hex[in[i] & 0xF];
+        }
+        return out;
     }
    
 };
