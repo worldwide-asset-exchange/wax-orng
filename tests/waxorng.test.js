aa92eddcac5267ea4f94706a2aaf509fe7888859
Long Vu <hlongvu@gmail.com>
2025-06-23 15:24:57 +0700

    add onerror

--- tests/waxorng.test.js
+++ tests/waxorng.test.js
@@ -309,6 +309,30 @@ describe('test orng smart contract', () => {
 
       expect(configTable4.rows[0].value).toBe(10);
     });
+
+    it('should disable deferred transactions for testing', async () => {
+      await orngContract.contract.action.setconfig(
+        {
+          config: 'usedeferred',
+          value: 0,
+        },
+        [
+          {
+            actor: orngContract.name,
+            permission: 'active',
+          },
+        ]
+      );
+
+      const configTable = await orngContract.contract.table['config.a'].get({
+        scope: orngContract.name,
+        lower_bound: 'usedeferred',
+        upper_bound: 'usedeferred',
+      });
+
+      expect(configTable.rows.length).toBe(1);
+      expect(configTable.rows[0].value).toBe(0);
+    });
   });
 
   describe('set publickey tests', () => {
